7e84e41567ec6a913653c4cfd7cbb7172e8b52ce
Nick Moore <nicholas.moore5@icloud.com>
2018-06-19 17:20:15 -0400

    Changed vars to consts, functions to arrow functions

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // This program will randomly display a quote every time the "Show another quote" button is clicked
 
-var quotes = [
+const quotes = [
 {quote : 'And so it goes...', source : 'Kurt Vonnegut', citation : 'Slaughterhouse-Five', year : '1972', tag : 'Books'},
 {quote : 'Your body is not a temple, it\'s an amusement park. Enjoy the ride.', source : 'Anthony Bourdain', citation : 'Kitchen Confidential: Adventures in the Culinary Underbelly', year : '2000', tag : 'Books'},
 {quote : 'Books are a poor substitute for female companionship, but they are easier to find.', source : 'Patrick Rothfuss', citation : 'The Name of the Wind', year : '2007', tag : 'Books'},
@@ -9,21 +9,17 @@ var quotes = [
 {quote : 'Wubba lubba dub dub!', source : 'Rick Sanchez', citation : 'Rick and Morty', tag : 'TV Shows'},
 {quote : 'Winter is coming.', source : 'Eddard Stark', citation : 'Game of Thrones', tag : 'TV Shows'}
 ];
-var intervalID = window.setInterval(printQuote, 30000);
+const intervalID = window.setInterval(printQuote, 30000);
 
 // selects random quote object from quote array and returns
-function getRandomQuote(){
-  return quotes[Math.floor(Math.random() * quotes.length)];
-}
+const getRandomQuote = () => quotes[Math.floor(Math.random() * quotes.length)];
 
 //Displays random color each time quote changes
-function randomRGB() {
-    return Math.floor(Math.random() * 100);
-}
+const randomRGB = () => Math.floor(Math.random() * 100);
 
 // calls getRandomQuote and stores return quote in variable
 // constructs a string containing properties of quote and displays in index.html when called
-function printQuote(){
+function printQuote() {
   var randomQuote = getRandomQuote();
   var rgbColor = 'rgb(' + randomRGB() + ',' + randomRGB() + ',' + randomRGB() + ')';
   var quote = ' ';
